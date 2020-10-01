feea70ac42bed39f903602da33c723d2f828f93b
Leonard v.V <l.vanvelzen16@web.de>
2020-10-01 14:53:29 +0200

    utils and basic constants like status_codes and status_messages

---
+++ src/constants/Code.ts
@@ -0,0 +1,11 @@
+export class Code {
+
+    static readonly ROUTE_NOT_FOUND = 404;
+
+    static readonly MISSING_PARAMETERS = 100;
+    static readonly MISSING_PERMISSIONS = 100;
+
+    static readonly EVENT_NOT_FOUND = 404;
+    static readonly PLACE_NOT_FOUND = 404;
+
+}
